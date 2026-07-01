4dd4262a4ad47f122e05a44e75029f5602f7a37c
harshyadav37 <hy050712@gmail.com>
2026-07-01 18:11:23 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Page = () => {
 const router=useRouter();
   return (
     <div className="w-full min-h-screen bg-white">
-      {/* HERO SECTION jhhj*/}
+      {/* HERO SECTION */}
       <section className="relative">
         <Image
           src={landscape1}
